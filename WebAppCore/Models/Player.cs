d3b62b25a63a72c82649c15244376210c1f2d05c
HARSHINIGOSU <harshinigosu711@gmail.com>
2022-07-06 16:32:18 +0530

    Second Commit

---
+++ WebAppCore/Models/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppCore.Models
+{
+    public class Player
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public string PTeam {get;set;}
+
+        public double PAvg { get; set;}
+    }
+}
